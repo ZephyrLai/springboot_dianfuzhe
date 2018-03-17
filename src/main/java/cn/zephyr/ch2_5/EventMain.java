4946cc984056e1c1afae4e932bb897e520ba8f8a
ZephyrLai <laizonghao@foxmail.com>
2018-03-17 17:21:15 +0800

    Application Event 事件监听

---
+++ src/main/java/cn/zephyr/ch2_5/EventMain.java
@@ -0,0 +1,19 @@
+package cn.zephyr.ch2_5;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @Author: Zephyr
+ * @Description:
+ * @Date: 2018/3/17 17:15
+ */
+public class EventMain {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(EventConfig.class);
+        MyPublisher publisher = context.getBean(MyPublisher.class);
+        publisher.publish("haha");
+
+    }
+}
